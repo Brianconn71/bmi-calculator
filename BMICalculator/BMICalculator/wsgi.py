4e58ca37497fc57183a6c5e9bfd542e924a772b1
Brian Connolly <brian.connolly71@gmail.com>
2024-07-02 17:32:01 +0100

    Oh my god, shhh, shhh, shut up. Its working, its working! Thank you docker overlords

--- BMICalculator/BMICalculator/wsgi.py
+++ BMICalculator/BMICalculator/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BMICalculator.BMICalculator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BMICalculator.settings')
 
 application = get_wsgi_application()
 
